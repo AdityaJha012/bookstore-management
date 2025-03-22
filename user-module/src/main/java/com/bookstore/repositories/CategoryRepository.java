f8409c3f70388fc22eec6662b036be0f3b876497
Aditya Kumar <adityajha479@gmail.com>
2025-03-22 22:11:01 +0530

    Delete old modules

--- user-module/src/main/java/com/bookstore/repositories/CategoryRepository.java
+++
@@ -1,8 +0,0 @@
-package com.bookstore.repositories;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import com.bookstore.models.BookCategory;
-
-public interface CategoryRepository extends JpaRepository<BookCategory, Integer>
-{
-}
--- user-module/src/main/java/com/bookstore/repositories/PublisherRepository.java
+++
@@ -1,8 +0,0 @@
-package com.bookstore.repositories;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import com.bookstore.models.BookPublisher;
-
-public interface PublisherRepository extends JpaRepository<BookPublisher, Integer>
-{
-}
